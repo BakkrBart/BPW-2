67b9d615116745b03feefc8cf9f5fcf8b5350bab
Bart <bartvanlierop.bvl@gmail.com>
2021-03-04 12:37:16 +0100

    Enemy spawning

--- Assets/Scripts/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGenerator.cs
@@ -2,148 +2,189 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DungeonGenerator : MonoBehaviour
+namespace roguelike.generator
 {
-    public enum Tile { floor }
-
-    [Header("Prefabs")]
-    public GameObject FloorPrefab;
-    public GameObject WallPrefab;
-
-    [Header("DungeonSettings")]
-    public int amountRooms;
-    public int width = 100;
-    public int height = 100;
-    public int minRoomSize = 3;
-    public int maxRoomSize = 7;
-
-    private Dictionary<Vector2Int, Tile> dungeonDictionary = new Dictionary<Vector2Int, Tile>();
-    private List<Room> roomList = new List<Room>();
-    private List<GameObject> allSpawnedObjects = new List<GameObject>();
-
-    void Start()
+    public class DungeonGenerator : MonoBehaviour
     {
-        GenerateDungeon();
-    }
+        public enum Tile { floor }
 
-    private void AllocateRooms()
-    {
-        for (int i = 0; i < amountRooms; i++)
+        [Header("Prefabs")]
+        public GameObject FloorPrefab;
+        public GameObject WallPrefab;
+        public GameObject EnemyPrefab;
+        public GameObject Player;
+
+        [Header("DungeonSettings")]
+        public int amountRooms;
+        public int width = 100;
+        public int height = 100;
+        public int minRoomSize = 3;
+        public int maxRoomSize = 7;
+        public int enemiesInRoom = 2;
+        public int seed;
+
+        private Dictionary<Vector2Int, Tile> dungeonDictionary = new Dictionary<Vector2Int, Tile>();
+        private List<Room> roomList = new List<Room>();
+        private List<GameObject> allSpawnedObjects = new List<GameObject>();
+
+        void Start()
         {
-            Room room = new Room()
-            {
-                position = new Vector2Int(Random.Range(0, width), Random.Range(0, height)),
-                size = new Vector2Int(Random.Range(minRoomSize, maxRoomSize), Random.Range(minRoomSize, maxRoomSize))
-            };
-
-            if (CheckIfRoomFitsDungeon(room))
-            {
-                AddRoomToDungeon(room);
-            }
-            else
-            {
-                i--;
-            }
+            seed = Random.Range(0, 1000);
+            Random.InitState(seed);
+            GenerateDungeon();
         }
-    }
 
-    private void AddRoomToDungeon(Room room)
-    {
-        for (int xx = room.position.x; xx < room.position.x + room.size.x; xx++)
+        private void AllocateRooms()
         {
-            for (int yy = room.position.y; yy < room.position.y + room.size.y; yy++)
+            for (int i = 0; i < amountRooms; i++)
             {
-                Vector2Int pos = new Vector2Int(xx, yy);
-                dungeonDictionary.Add(pos, Tile.floor);
-            }
-        }
-        roomList.Add(room);
-    }
-
-    private bool CheckIfRoomFitsDungeon(Room room)
-    {
-        for (int xx = room.position.x; xx < room.position.x + room.size.x; xx++)
-        {
-            for (int yy = room.position.y; yy < room.position.y + room.size.y; yy++)
-            {
-                Vector2Int pos = new Vector2Int(xx, yy);
-                if (dungeonDictionary.ContainsKey(pos))
+                Room room = new Room()
                 {
-                    return false;
+                    position = new Vector2Int(Random.Range(0, width), Random.Range(0, height)),
+                    size = new Vector2Int(Random.Range(minRoomSize, maxRoomSize), Random.Range(minRoomSize, maxRoomSize))
+                };
+                Room startRoom = new Room()
+                {
+                    position = new Vector2Int(Random.Range(0, width), Random.Range(0, height)),
+                    size = new Vector2Int(Random.Range(minRoomSize, maxRoomSize), Random.Range(minRoomSize, maxRoomSize))
+                };
+
+                if (CheckIfRoomFitsDungeon(room))
+                {
+                    AddRoomToDungeon(room);
+                }
+                else
+                {
+                    i--;
                 }
             }
         }
-        return true;
-    }
 
-    private void AllocateCorridors()
-    {
-        for (int i = 0; i < roomList.Count; i++)
+        private void AddRoomToDungeon(Room room)
         {
-            Room startRoom = roomList[i];
-            Room otherRoom = roomList[(i + Random.Range(1, roomList.Count - 1)) % roomList.Count];
-
-            int dirX = Mathf.RoundToInt(Mathf.Sign(otherRoom.position.x - startRoom.position.x));
-            for (int x = startRoom.position.x; x != otherRoom.position.x; x += dirX)
+            for (int xx = room.position.x; xx < room.position.x + room.size.x; xx++)
             {
-                Vector2Int pos = new Vector2Int(x, startRoom.position.y);
-                if (!dungeonDictionary.ContainsKey(pos))
+                for (int yy = room.position.y; yy < room.position.y + room.size.y; yy++)
                 {
+                    Vector2Int pos = new Vector2Int(xx, yy);
                     dungeonDictionary.Add(pos, Tile.floor);
                 }
             }
-            
-            int dirY = Mathf.RoundToInt(Mathf.Sign(otherRoom.position.y - startRoom.position.y));
-            for (int y = startRoom.position.y; y != otherRoom.position.y; y += dirY)
+            roomList.Add(room);
+        }
+
+        private bool CheckIfRoomFitsDungeon(Room room)
+        {
+            for (int xx = room.position.x; xx < room.position.x + room.size.x; xx++)
             {
-                Vector2Int pos = new Vector2Int(otherRoom.position.x, y);
-                if (!dungeonDictionary.ContainsKey(pos))
+                for (int yy = room.position.y; yy < room.position.y + room.size.y; yy++)
                 {
-                    dungeonDictionary.Add(pos, Tile.floor);
+                    Vector2Int pos = new Vector2Int(xx, yy);
+                    if (dungeonDictionary.ContainsKey(pos))
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private void AllocateCorridors()
+        {
+            for (int i = 0; i < roomList.Count; i++)
+            {
+                Room startRoom = roomList[i];
+                Room otherRoom = roomList[(i + Random.Range(1, roomList.Count - 1)) % roomList.Count];
+
+                int dirX = Mathf.RoundToInt(Mathf.Sign(otherRoom.position.x - startRoom.position.x));
+                for (int x = startRoom.position.x; x != otherRoom.position.x; x += dirX)
+                {
+                    Vector2Int pos = new Vector2Int(x, startRoom.position.y);
+                    if (!dungeonDictionary.ContainsKey(pos))
+                    {
+                        dungeonDictionary.Add(pos, Tile.floor);
+                    }
+                }
+
+                int dirY = Mathf.RoundToInt(Mathf.Sign(otherRoom.position.y - startRoom.position.y));
+                for (int y = startRoom.position.y; y != otherRoom.position.y; y += dirY)
+                {
+                    Vector2Int pos = new Vector2Int(otherRoom.position.x, y);
+                    if (!dungeonDictionary.ContainsKey(pos))
+                    {
+                        dungeonDictionary.Add(pos, Tile.floor);
+                    }
+                }
+            }
+        }
+
+        private void BuildDungeon()
+        {
+            foreach (KeyValuePair<Vector2Int, Tile> kv in dungeonDictionary)
+            {
+                GameObject floor = Instantiate(FloorPrefab, new Vector3Int(kv.Key.x, 0, kv.Key.y), Quaternion.identity);
+                allSpawnedObjects.Add(floor);
+
+                SpawnWallsForTile(kv.Key);
+            }
+        }
+
+        private void SpawnWallsForTile(Vector2Int position)
+        {
+            for (int x = -1; x <= 1; x++)
+            {
+                for (int z = -1; z <= 1; z++)
+                {
+                    if (Mathf.Abs(x) == Mathf.Abs(z)) { continue; }
+                    Vector2Int gridPos = position + new Vector2Int(x, z);
+                    if (!dungeonDictionary.ContainsKey(gridPos))
+                    {
+                        Vector3 direction = new Vector3(gridPos.x, 0, gridPos.y) - new Vector3(position.x, 0, position.y);
+                        GameObject wall = Instantiate(WallPrefab, new Vector3(position.x, 0, position.y), Quaternion.LookRotation(direction));
+                        allSpawnedObjects.Add(wall);
+                    }
+                }
+            }
+        }
+
+        public void GenerateDungeon()
+        {
+            AllocateRooms();
+            AllocateCorridors();
+            BuildDungeon();
+            PoppulateDungeon();
+        }
+
+        private void PoppulateDungeon()
+        {
+            foreach (var startRoom in roomList)
+            {
+                Player.transform.position = startRoom.middlePos();
+            }
+            foreach (var room in roomList)
+            {
+                for (int i = 0; i < enemiesInRoom; i++)
+                {
+                    GameObject enemy = Instantiate(EnemyPrefab, room.randomPos(), Quaternion.LookRotation(new Vector3(0,0,0)));
+                    allSpawnedObjects.Add(enemy);
                 }
             }
         }
     }
 
-    private void BuildDungeon()
+    public class Room
     {
-        foreach(KeyValuePair<Vector2Int, Tile> kv in dungeonDictionary)
-        {
-            GameObject floor = Instantiate(FloorPrefab, new Vector3Int(kv.Key.x, 0, kv.Key.y), Quaternion.identity);
-            allSpawnedObjects.Add(floor);
+        public Vector2Int position;
+        public Vector2Int size;
 
-            SpawnWallsForTile(kv.Key);
+        public Vector3 randomPos()
+        {
+            return new Vector3(position.x + Random.Range(0, size.x), 0, position.y + Random.Range(0, size.y));
+        }
+
+        public Vector3 middlePos()
+        {
+            return new Vector3(position.x + (size.x / 2), 0, position.y + (size.y / 2));
         }
     }
-
-    private void SpawnWallsForTile(Vector2Int position)
-    {
-        for (int x = -1; x <= 1; x++)
-        {
-            for (int z = -1; z <= 1; z++)
-            {
-                if(Mathf.Abs(x) == Mathf.Abs(z)) { continue; }
-                Vector2Int gridPos = position + new Vector2Int(x, z);
-                if (!dungeonDictionary.ContainsKey(gridPos))
-                {
-                    Vector3 direction = new Vector3(gridPos.x, 0, gridPos.y) - new Vector3(position.x, 0, position.y);
-                    GameObject wall = Instantiate(WallPrefab, new Vector3(position.x, 0, position.y), Quaternion.LookRotation(direction));
-                    allSpawnedObjects.Add(wall);
-                }
-            }
-        }
-    }
-
-    public void GenerateDungeon()
-    {
-        AllocateRooms();
-        AllocateCorridors();
-        BuildDungeon();
-    }
-}
-
-public class Room
-{
-    public Vector2Int position;
-    public Vector2Int size;
 }
